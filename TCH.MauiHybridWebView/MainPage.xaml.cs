1eb23e52662c94906de4e6e08e8f352b63f7189e
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2024-10-30 20:06:58 +0100

    Add project files.

---
+++ TCH.MauiHybridWebView/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace TCH.MauiHybridWebView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            hybridWebView.SendRawMessage(messageForJS.Text);
+        }
+
+        private void hybridWebView_RawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
+        {
+            messagesFromJs.Text = e.Message + Environment.NewLine + messagesFromJs.Text;
+        }
+    }
+
+}
